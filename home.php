66beef9d6a759dd2b3d7fcbff296547ecb8a1084
Akhil001 <akhilkarri2002@gmail.com>
2023-04-26 02:44:36 -0700

    read update

--- home.php
+++ home.php
@@ -29,6 +29,8 @@ if (isset($_SESSION["user_id"])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
     <link rel="icon" href="logo.png">
     <title>Home</title>
@@ -41,11 +43,24 @@ if (isset($_SESSION["user_id"])) {
         } 
     </style>
     
+<?php    
+     
+if(isset($_GET['delid'])) {    
+        $id = $_GET['delid'];       
+        $sql_delete = "DELETE FROM `documents` WHERE id = '$id' ";
+        $result = $mysqli->query($sql_delete);      
+        if($result){
+            header('Location: home.php');
+        }else{
+            echo "Error";
+        }
+}
+?>  
+    
 </head>
 <body>
-
-       
-            <?php if (isset($user)): ?> 
+     
+<?php if (isset($user)): ?> 
 
     <!-- navbar -->
     <header>
@@ -85,43 +100,34 @@ if (isset($_SESSION["user_id"])) {
     <div class="container">
         <h1>Documents</h1>
     </div>
-    
 
-    
+ 
     <div class="container" style="padding-bottom: 150px; margin-top: 100px; margin-bottom: 100px; border-radius: 20px; padding-left: 140px; padding-right: 140px">
-        
 
-            
-            
             
 <?php
 $result = $mysqli->query("SELECT * FROM `documents`");
 if (mysqli_num_rows($result) > 0) {
-    
-    
-    while($row = mysqli_fetch_assoc($result)){     ?>
-        
-
-                    
-        <div class="container" style="padding: 30px; border: #AA96DA solid; border-radius: 20px; background-color: #73db9038; margin-bottom: 50px ">
+    while($row = mysqli_fetch_assoc($result)){     
+?>
+                
+        <div class="container" style="padding: 30px; border: #AA96DA solid; border-radius: 20px; background-color: #73db9038; margin-bottom: 50px ">    
             <div class="row" style="  ">
               <div class="col-sm-8" >
                   <h2> <?php echo $row['title'] ?> </h2>
                   <p>  </p>
-                  <p class="text-right"> By <?php  echo $row['author'] ?> </p> 
                   <p>Writen on <?php echo $row['createdOn'] ?>  </p>
-                  
+                  <p class="text-right"> By <?php  echo $row['author'] ?> </p> 
               </div>
 
               <div class="col-sm-4" >
-                  <a href='home.php?rid=<?php echo $row['id']; ?>' class="btn btn-outline-success mr-2 btn-sm float-right read" >READ</a><br><br>
-                  <a href='home.php?aid=<?php echo $row['id']; ?>' class="btn btn-outline-primary mr-2 btn-sm float-right read" style="float: right!important;  ">EDIT</a><br><br>
-                  <a href='home.php?delid=<?php echo $row['id']; ?>' class="btn btn-outline-danger mr-2 btn-sm float-right read" style=" float: right!important;">DELETE</a><br><br>
-                  
+                  <a href='read.php?rid=<?php echo $row['id']; ?>' class="btn btn-outline-success mr-2 btn-sm float-right read" >READ</a><br><br>
+                  <a href='update.php?uid=<?php echo $row['id']; ?>' class="btn btn-outline-primary mr-2 btn-sm float-right read" style="float: right!important;  ">EDIT</a><br><br>
+<!--                  <a href='home.php?aid=<?php echo $row['id']; ?>' class="btn btn-outline-primary mr-2 btn-sm float-right read" style="float: right!important;  ">EDIT</a><br><br>-->
+                  <a href='home.php?delid=<?php echo $row['id']; ?>' class="btn btn-outline-danger mr-2 btn-sm float-right read" style=" float: right!important;">DELETE</a><br><br>               
               </div>
             </div>
-        </div>
-            
+        </div>    
 <?php        
             
     }
@@ -131,27 +137,8 @@ if (mysqli_num_rows($result) > 0) {
 ?>               
              
         
-        <div class="container" style="padding: 30px; border: #AA96DA solid; border-radius: 20px; background-color: #73db9038; margin-bottom: 50px ">
-            <div class="row" style=" ">
-              <div class="col-sm-8" >
-                  <h3>Women in Hinduism</h3>
-                  <p>  </p>
-                  <p>Writen on 12-4-56 </p>
-                  <p class="text-right"> By Author</p>
-              </div>
-              <div class="col-sm-4" >
-                  <a href='createDoc.php?aid=1' class="btn btn-outline-success mr-2 btn-sm float-right read" style="float: right!important;  ">READ</a><br><br>
-                  <a href='createDoc.php?aid=1' class="btn btn-outline-primary mr-2 btn-sm float-right read" style="float: right!important;  ">EDIT</a><br><br>
-                  <a href='createDoc.php?aid=1' class="btn btn-outline-danger mr-2 btn-sm float-right read" style=" float: right!important;">DELETE</a><br><br>
-              </div>
-            </div>
-        </div>
 
-        
     </div>
-
-    
-
 
     <?php else: ?>
         <header>
@@ -160,7 +147,6 @@ if (mysqli_num_rows($result) > 0) {
                 <a href="Log-In.php" class="navbar-logo">                 </a>
                      <img class="d-inline-block align-top" src="logo.png" height="40" alt=""> 
                     <a href="Log-In.php" class="nav-link" style="padding-left: 20px;"> Virtual Office in Cloud</a>
-
                 <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarNav"> <span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto">
@@ -170,9 +156,7 @@ if (mysqli_num_rows($result) > 0) {
                         <li class="nav-item active">
                             <a href="contact.html" class="nav-link"> Contact </a>
                         </li>
-
                     </ul>
-
                 </div>
             </div>
         </nav>
@@ -181,11 +165,6 @@ if (mysqli_num_rows($result) > 0) {
     <p style="text-align: center; padding-top: 100px"><a href="Log-In.php">Log-in</a> or <a href="Sign-Up.html">sign-up</a></p>
         
     <?php endif; ?>
-
-
-
-
-
 
     <footer class="bg-light text-center text-lg-start" style="margin-top: 200px; bottom: 0;">
   <div class="text-center p-3" style="background-color: #7ddf99a5;; ">
