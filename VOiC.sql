b3b76644ec4aca25cc566cce8470a8a5e17365b7
AkhilKarri2002 <akhilkarri2002@gmail.com>
2023-04-30 14:44:58 -0700

    user privileges

--- VOiC.sql
+++ VOiC.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 30, 2023 at 10:26 AM
+-- Generation Time: Apr 30, 2023 at 11:44 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -52,11 +52,9 @@ CREATE TABLE `documents` (
 --
 
 INSERT INTO `documents` (`id`, `title`, `description`, `author`, `createdOn`, `todaydate`, `ChildName`, `ChildState`, `Parent1Name`, `ChildStayPre`, `Parent1State`, `CaseDate`, `CaseState`, `ChangeState`, `Parent2Name`, `CaseStay`, `Parent2State`) VALUES
-(5, 'Women in Hinduism', '<ul><li><i><strong>Women have played a significant role in Hinduism since its inception. Hinduism is one of the oldest religions in the world, with a history that dates back more than 5,000 years. Throughout this long history, Hinduism has evolved in many ways, but its teachings have always held women in high regard.&nbsp;</strong></i></li></ul><ol><li><i><strong>This essay will explore the role of women in Hinduism, including their place in the religious texts, their social status, and the challenges they have faced and continue to face. Role of Women in Hindu Religious Texts Hinduism has a vast collection of sacred texts, including the Vedas, the Upanishads, and the Bhagavad Gita.&nbsp;</strong></i></li><li><i><strong>These texts form the basis of Hinduism and contain a wealth of information about the religion\'s teachings and practices. Women play an important role in these texts, and their stories and experiences are woven throughout the narratives.</strong></i></li></ol>', 'Akhil', '2023-04-25', '0000-00-00', '', '', '', 0, '', '0000-00-00', '', '', '', 0, ''),
-(7, 'Computer Science', '<ol><li><strong>Initially Computer Science seems hard because learning to program is challenging. Programming is the first task that Computer Science students must master, and programming requires an extremely logical and methodical approach to solving problems. Students who are weak in mathematics often have to work harder to attain the logical thinking skills necessary to learn to program.</strong></li><li><strong>Programming is introduced \"cold\" to students in a first computer science course and continued in the next several courses. Compared to Mathematics, students learn math starting in kindergarten and continuing all the way through college. It is introduced in small steps, all the way through school. Programming is a similar intellectual skill that takes time to master, usually in about 4-5 courses.</strong></li><li><strong>Some students appear to find programming easy and unnecessarily intimidate others into believing they are not suited to computer science. However, most of people learn skills step-by-step over time. Can anyone who has no background in music learn to play a musical instrument really well in one semester? Can someone starting from scratch learn to speak a foreign language fluently with a single course? Unless you are a musical genius, or a young child living in a bi-lingual family, the answer for the vast majority of people is no.</strong></li><li><strong>You can almost think of learning to program as equivalent to learning to speak &amp; write in Chinese and Russian at the same time from scratch. Put another way, you can almost think of learning to program as equivalent to taking biochemistry, gross anatomy, physiology, pharmacology, and pathology courses in a MD program at a medical school (these are 6+ credit hour courses that expect you to memorize incredible amounts of information). The difference is that CS courses require you to do the programming to learn the skills, as opposed to memorizing large quantities of information or spending many hours rotely repeating language phrases and idioms.</strong></li></ol>', 'Professor', '2023-04-26', '0000-00-00', '', '', '', 0, '', '0000-00-00', '', '', '', 0, ''),
-(11, 'Database Management system (DBMS)', '<p>A database management system (DBMS) is system software for creating and managing databases. A DBMS makes it possible for end users to create, protect, read, update and delete data in a <a href=\"https://www.techtarget.com/searchdatamanagement/definition/database\">database</a>. The most prevalent type of <a href=\"https://www.techtarget.com/searchdatamanagement/definition/data-management\">data management</a> platform, the DBMS essentially serves as an interface between databases and users or application programs, ensuring that data is consistently organized and remains easily accessible.</p><h3><strong>What does a DBMS do?</strong></h3><p>The DBMS manages the data; the database engine allows data to be accessed, locked and modified; and the database <a href=\"https://www.techtarget.com/searchdatamanagement/definition/schema\">schema</a> defines the database\'s logical structure. These three foundational elements help provide concurrency, security, data integrity and uniform data administration procedures. The DBMS supports many typical database administration tasks, including change management, performance monitoring and tuning, security, and backup and recovery. Most database management systems are also responsible for automated rollbacks and restarts as well as logging and auditing of activity in databases and the applications that access them.</p><p>The DBMS provides a centralized view of data that can be accessed by multiple users from multiple locations in a controlled manner. A DBMS can limit what data end users see and how they view the data, providing many views of a single database schema. End users and software programs are free from having to understand where the data is physically located or on what type of storage medium it resides because the DBMS handles all requests.</p><p>The DBMS can offer both logical and physical data independence to protect users and applications from having to know where data is stored or from being concerned about changes to the physical structure of data. So long as programs use the application programming interface (API) for the database that the DBMS provides, developers won\'t have to modify programs just because changes have been made to the database.</p>', 'Student', '2023-04-26', '0000-00-00', '', '', '', 0, '', '0000-00-00', '', '', '', 0, ''),
-(12, 'Sample Demo', '<p>asdf;ihasodi fhaosdfj oasipdhf opaishdf opihasdf hasd;fh asidj fasojd fioasjd fashd fkhasdlfk hbsadguakljdbg asuildfh asidhfb kasdb fkasbdhf kashdf ahs dfh. ahsio dfhais dhfas fioa dhaso hfaso ihfasoifh oiasfh aopis hoaipsoiahs foihakj baskljbasfuih asiudhf auihe kjasbefkjawebf kjalbsf huasdi ofuhasiufh awekj bhfakljesbf klasjdbfh iasuhf ioawh efiouawef iouahsdf ihas&nbsp;</p>', 'Anon', '2023-04-26', '0000-00-00', '', '', '', 0, '', '0000-00-00', '', '', '', 0, ''),
-(16, 'Sample Doc with Attributes', '<p>This is just a sample doc with attriubutes to check how the system is working and test it.</p>', 'Attrib', '2023-04-30', '2023-04-14', 'child', 'AL', 'Parent 1', 24, 'IL', '2023-01-02', 'AK', 'FL', 'Parent 2', 35, 'CA');
+(17, '1234', '<p>1234</p>', '1243', '2023-04-30', '2023-04-30', '1234', 'AL', '1234', 1234, 'AL', '2023-04-30', 'AL', 'AL', '1234', 1234, 'AL'),
+(18, 'asdf', '<p>asdf</p>', 'asdf', '2023-04-30', '2023-03-31', 'asdf', 'CA', 'asdf', 789, 'CA', '2023-03-31', 'CA', 'CA', 'asdf', 789, 'CA'),
+(19, 'WHY IS IT HARD TO LEARN TO PROGRAM?', '<p><a href=\"https://www.lindenwood.edu/science/mathematics-computer-science-and-information-technology/computer-science/prospective-students/is-computer-science-hard/\">IS COMPUTER SCIENCE HARD?</a></p><h2><a href=\"https://slate.lindenwood.edu/apply/\">APPLY NOW</a></h2><h2><a href=\"https://slate.lindenwood.edu/register/request_info\">REQUEST INFO</a></h2><h2><a href=\"https://www.lindenwood.edu/admissions/visit-campus/\">SCHEDULE A VISIT</a></h2><h2>I HEARD THAT COMPUTER SCIENCE IS ONE OF THE HARDEST FIELDS OF STUDY. IS THAT TRUE?</h2><p>Yes. Computer Science is a hard discipline to learn. But, if you are motivated and devote sufficient time to studying the discipline, then it is possible to learn Computer Science.</p><h3>FULL ANSWER</h3><h2>WHY IS IT HARD TO LEARN TO PROGRAM?</h2><p>Computer Science is a hard discipline to learn because learning to program is hard. But, if you are motivated and devote sufficient time to studying the discipline, then it is possible to learn how to program.</p><h3>FULL ANSWER</h3><h2>WHAT DOES IT MEAN THAT THE DISCIPLINE OF COMPUTER SCIENCE IS CONSTRUCTIVE IN NATURE?</h2><p>Computer Science is a hard discipline to learn because of the constructive nature of the discipline. But, if you are motivated and devote sufficient time to studying the discipline, then it is possible to learn and master every concept when it is encountered.</p><h3>FULL ANSWER</h3><h2>HOW MUCH TIME IS REQUIRED TO SUCCESSFULLY LEARN COMPUTER SCIENCE?</h2><p>Even though Computer Science is a hard discipline to learn, if you are motivated and devote sufficient time to studying the discipline, then it is possible to learn Computer Science.</p><h2>BUT IF I HAVE TO PUT IN SO MUCH TIME TO STUDY COMPUTER SCIENCE, THEN I WON\'T HAVE TIME TO DO OTHER THINGS, WILL I?</h2><p>Computer Science requires students to invest a lot of time to learn. But, if you learn good time management skills, you can put in the necessary time to learn the discipline and still have time for other activities.</p><h3>FULL ANSWER</h3><p>&nbsp;</p><blockquote><p>Many students are not motivated or prepared to invest this amount of time. They are looking for an “easy” discipline that “allows them time for significant extra-curricular or social activities.” They don’t believe that they can have a fulfilling social life in college while putting in this amount of time to study. Fortunately, students who are motivated to invest the necessary time often find that they can also fit in active extra-curricular or social calendars and still be satisfied. The only difference is that CS students must utilize really good time management skills, and must plan for these extra-curricular and social activities.</p></blockquote>', 'Akhil', '2023-04-30', '1970-01-01', '', '', '', 0, '', '1970-01-01', '', '', '', 0, '');
 
 -- --------------------------------------------------------
 
@@ -68,16 +66,18 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password_hash` varchar(255) NOT NULL
+  `password_hash` varchar(255) NOT NULL,
+  `role` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `name`, `email`, `password_hash`) VALUES
-(1, 'asdf', 'asdf@asdf.com', '$2y$10$DWpKjTuQcI7VlggfbuD/jOcse8bETDT37SyvWXO37tsGfZ5GibajO'),
-(6, '1234', '1243@1243.com', '$2y$10$GuwfycUF6bR9OH82wNEqW.HHwtGHZe3T3Aguwnbq8Kc3PXPiNivae');
+INSERT INTO `user` (`id`, `name`, `email`, `password_hash`, `role`) VALUES
+(1, 'asdf', 'asdf@asdf.com', '$2y$10$DWpKjTuQcI7VlggfbuD/jOcse8bETDT37SyvWXO37tsGfZ5GibajO', 2),
+(7, 'qwer', 'qwre@asdf.com', '$2y$10$PT6PWYms8hjzszZyO9kbyeeAarOKNBv561g3O13/QDtREyg7PdMT6', 1),
+(8, '1234', '1234@1234.com', '$2y$10$kmD6IGk.kWd74X6XMHzLGukhroEjRH54O5Ytlb7RQqT..I1bykT7O', 1);
 
 --
 -- Indexes for dumped tables
@@ -104,13 +104,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `documents`
 --
 ALTER TABLE `documents`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ admin.php
@@ -0,0 +1,213 @@
+<?php
+session_start();
+if (isset($_SESSION["user_id"])  && ($_SESSION["role"]!= '2' )) {    
+    if ( $_SESSION["role"] != '2'  ) {
+                header("Location: home.php");
+                die();
+    }      
+    $mysqli = require __DIR__ . "/database.php";
+    $sql = "SELECT * FROM user
+            WHERE id = {$_SESSION["user_id"]}";           
+    $result = $mysqli->query($sql);    
+    $user = $result->fetch_assoc();
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="logo.png">
+    <title>Admin</title>
+
+    <style>
+        .read {
+            float: right!important;  
+
+            border-radius: 5px;
+        } 
+        .search-btn1 {
+            background: #AA96DA;
+            color: #21216f;
+            float: right;
+            width: 40px;
+            height: 40px;
+            border-radius: 20%;
+        /*    background: #73db90 ;*/
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            transition: 0.5s;            
+        }
+        .search-txt1 {
+            width: 240px;
+            padding: 0 6px;
+            border: none;
+            background: none;
+            outline: none;
+            float: left;
+            color: rgb(0, 0, 0);
+            font-size:  14px;
+            transition: 0.5s;
+            line-height:  40px;
+        }
+    </style>
+    
+<?php    
+     
+if(isset($_GET['delid'])) {    
+        $id = $_GET['delid'];       
+        $sql_delete = "DELETE FROM `documents` WHERE id = '$id' ";
+        $result = $mysqli->query($sql_delete);      
+        if($result){
+            header('Location: home.php');
+        }else{
+            echo "Error";
+        }
+}
+?>  
+    
+</head>
+<body>
+     
+<?php if (isset($user)): ?> 
+
+    <!-- navbar -->
+    <header>
+        <nav class="navbar navbar-expand-lg  navbar-light">
+            <div class="container">
+                <a href="VOiC.php" class="navbar-logo">
+                    <img class="d-inline-block align-top" src="logo.png" height="40" alt="">
+                </a>
+                
+                 <?php if($_SESSION["role"] == 2) {  ?>
+                    <a href="admin.php" class="nav-link" style="padding-left: 20px; font-weight: bolder; font-size: 18px; letter-spacing: 0.18em; "> Admin</a>
+                <?php } ?>   
+                <?php if($_SESSION["role"] == 1) {  ?>
+                    <a href="VOiC.php" class="nav-link" style="padding-left: 20px; font-size: 18px;"> Virtual Office in Cloud</a>
+                <?php } ?>   
+
+<!--                <div class="search-box">
+                    <form action="search.php" method="post"  class="search-opt" style="all: unset;padding: none !important;" >
+                        <input class="search-txt1" type="text" name="search" placeholder="Type to Search">
+                        <button type="submit" name="submit-search" class="search-btn1" style="border: none;    "><i class="bi bi-search"></i></button>
+                    </form>
+                    
+                </div>-->
+                    
+                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarNav"> <span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto"> 
+                        <li class="nav-item active">
+                            <a href="home.php" class="nav-link">Manage Docs</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a href="create.php" class="nav-link">Create</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a href="VOiC.php" class="nav-link">Search</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a href="logout.php" class="nav-link"> Log-out </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!-- Navbar-End -->
+    
+    
+    <div class="container">
+        <h1>Manage Users</h1>
+    </div>
+
+    <div class="container" style="padding-bottom: 150px; margin-top: 100px; margin-bottom: 100px; border-radius: 20px; padding-left: 140px; padding-right: 140px">
+
+            
+<?php
+$result = $mysqli->query("SELECT * FROM `documents`");
+if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)){     
+?>
+                
+        <div class="container" style="padding: 30px; border: #AA96DA solid; border-radius: 20px; background-color: #73db9038; margin-bottom: 50px ">    
+            <div class="row" style="  ">
+              <div class="col-sm-8" >
+                  <h2> <?php echo $row['title'] ?> </h2>
+                  <p>  </p>
+                  <p>Writen on <?php echo $row['createdOn'] ?>  </p>
+                  <p class="text-right"> By <?php  echo $row['author'] ?> </p> 
+              </div>
+
+              <div class="col-sm-4" >
+                  <a href='read.php?rid=<?php echo $row['id']; ?>' class="btn btn-outline-success mr-2 btn-sm float-right read" >READ</a><br><br>
+                  <a href='update.php?uid=<?php echo $row['id']; ?>' class="btn btn-outline-primary mr-2 btn-sm float-right read" style="float: right!important;  ">EDIT</a><br><br>
+<!--                  <a href='home.php?aid=<?php echo $row['id']; ?>' class="btn btn-outline-primary mr-2 btn-sm float-right read" style="float: right!important;  ">EDIT</a><br><br>-->
+               <?php if($_SESSION["role"] == 2) {  ?>
+                  <a href='home.php?delid=<?php echo $row['id']; ?>' class="btn btn-outline-danger mr-2 btn-sm float-right read" style=" float: right!important;">DELETE</a><br><br>               
+               <?php } ?>
+              </div>
+            </div>
+        </div>    
+<?php        
+            
+    }
+} else {
+    echo "No results found.";
+}           
+?>               
+             
+        
+
+    </div>
+
+    <?php else: ?>
+        <header>
+        <nav class="navbar navbar-expand-md  navbar-light">
+            <div class="container">
+                <a href="Log-In.php" class="navbar-logo">                 </a>
+                     <img class="d-inline-block align-top" src="logo.png" height="40" alt=""> 
+                    <a href="Log-In.php" class="nav-link" style="padding-left: 20px;"> Virtual Office in Cloud</a>
+                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarNav"> <span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item active">
+                            <a href="#" class="nav-link"> About </a>
+                        </li>
+                        <li class="nav-item active">
+                            <a href="contact.html" class="nav-link"> Contact </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+        
+    <p style="text-align: center; padding-top: 100px"><a href="Log-In.php">Log-in</a> or <a href="Sign-Up.html">sign-up</a></p>
+        
+    <?php endif; ?>
+
+    <footer class="bg-light text-center text-lg-start" style="margin-top: 200px; bottom: 0;">
+  <div class="text-center p-3" style="background-color: #7ddf99a5;; ">
+    © 2023 Copyright
+    <a class="text-dark" href="https://github.com/AkhilKarri2002">Created by Akhil Karri</a>
+  </div>
+</footer>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>
